38155086a7c259e6dd36dc9e02389e89873a4225
ShehrozAhmadDev <shehrozahmad970@gmail.com>
2023-12-26 15:00:36 +0500

    auth routes done

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { userFind, userExists, validateEmail } from "../helpers";
 import { User } from "../models";
-import { hash } from "bcrypt";
 import { IUserDocument } from "../types/types";
 
 export const login = async (req: Request, res: Response) => {
@@ -65,7 +64,7 @@ export const signUp = async (req: Request, res: Response) => {
 
   return res.status(200).json({
     status: 200,
-    message: `User signed up successfully, email has been sent to ${email} for verification`,
+    message: `User signed up successfully, ${email}`,
     user: user
   });
 };
@@ -81,53 +80,4 @@ export const myProfile = async (
   return res.status(200).json({ user });
 };
 
-export const forgotPassword = async (req: Request, res: Response) => {
-  const { email }: { email: string } = req?.body;
 
-  if (!(await userExists(email))) {
-    return res.status(500).json({
-      message: `User with this email ${email} is not exists, please signup`,
-    });
-  }
-  const user = await User.findOne({ email });
-
-
-  return res.status(200).json({
-    status: 200,
-    message: `A reset verification link is sent to your email ${email}`,
-    user: user
-  });
-};
-
-export const verifyEmail = async (req: Request, res: Response) => {
-  const user = await userFind({
-    email: req?.user?.email.toLowerCase(),
-  });
-  if (user?.verified) {
-    return res.status(500).json({ message: `Your email is already verified` });
-  }
-  await User.findByIdAndUpdate(req?.user?._id, { verified: true });
-  return res
-    .status(200)
-    .json({ status: 200, message: "User verified successfully!" });
-};
-
-export const resetPassword = async (req: Request, res: Response) => {
-  if (!req?.body?.password) {
-    return res.status(500).json({
-      message: `Please enter new password`,
-    });
-  }
-  await User.findOneAndUpdate(
-    { email: req?.user?.email },
-    {
-      $set: {
-        password: await hash(req?.body?.password, 10),
-      },
-    }
-  );
-
-  return res
-    .status(200)
-    .json({ status: 200, message: "Reset password successful!" });
-};
