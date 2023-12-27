906132e971f83cc30c724ce50152f1b9486d6c62
ShehrozAhmadDev <shehrozahmad970@gmail.com>
2023-12-27 11:13:25 +0500

    feat: cors policy added

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import database from "./database";
 import { blue, bold, yellow } from "colors";
 import bodyParser from "body-parser";
 import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express();
 const PORT: number = parseInt(config.PORT as string, 10);
@@ -14,14 +15,21 @@ const server = http.createServer(app);
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //Initialize Routes
-app.use("/api", apiRoutes);
+
+
+//Logger
+morgan(':method :url :status :res[content-length] - :response-time ms')
+
 
 // Cors Policy
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', optionsSuccessStatus: 204 }));
+app.options('*', cors());
 
 
 //Database Connection
 database();
+
+app.use("/api", apiRoutes);
 
 //Listening to PORT
 server.listen(PORT, (): void =>
