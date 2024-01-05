308080f75241337dbc87fddeeb88f4b80a029919
ShehrozAhmadDev <shehrozahmad970@gmail.com>
2024-01-05 18:55:34 +0500

    feat: create convo fixed

--- src/controllers/conversation.controller.ts
+++ src/controllers/conversation.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import { Conversation } from "../models";
+import { adminFind } from "../helpers";
 
 /**
  * Creates new instance of Conversation in database
  */
 export const createConversation = async (req: Request, res: Response) => {
-  const { senderId, receiverId } = req.body;
+  const { senderId } = req.body;
+  const adminAccount =await adminFind();
 
-    // Check if a conversation already exists with the same members
+  // Check if a conversation already exists with the same members
+  if(adminAccount){
     const existingConversation = await Conversation.findOne({
-      members: { $all: [senderId, receiverId] },
+      members: { $all: [senderId, adminAccount._id] },
     });
 
     if (existingConversation) {
@@ -19,20 +22,17 @@ export const createConversation = async (req: Request, res: Response) => {
         conversation: existingConversation,
       });
     }
-
     // Conversation doesn't exist, create a new one
     const newConversation = new Conversation({
-      members: [senderId, receiverId],
+      members: [senderId, adminAccount._id],
     });
-
     const savedConversation = await newConversation.save();
-
     res.status(200).json({
       message: 'Conversation created successfully',
       conversation: savedConversation,
     });
   };
-  
+}
 
 /**
  * Find conversations of user from database
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,8 @@ import validator from "validator";
 
 export const userFind = (data: object) => User.findOne(data).populate("subscriptionId");
 
+export const adminFind = () => User.findOne({role: "admin"}).populate("subscriptionId");
+
 export const userFindById = (id?: string) => User.findById(id).select('-password').populate("subscriptionId");
 
 export const userExists = (email: string) => User.exists({ email });
